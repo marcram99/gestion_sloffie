6e1182c16ce021398fec4545813f86ad3c12d372
Marc Wanner <marcram99@gmail.com>
2023-06-29 16:53:35 +0200

    work on pdf facture

--- facture_pdf.py
+++ facture_pdf.py
@@ -2,32 +2,50 @@ from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4
 
 w, h = A4
-
+step = 13
+# Données facture
+no_fact = "FSMCC_00001"
+date_fact = "31.12.2023"
+client = {'nom':'Wanner',
+          'prenom':'Marc',
+          'adresse':"141 ch. de l'Eau Belle",
+          'code_postal':'74100',
+          'ville': 'Etrembières',
+          'mail': 'marcram@proton.me',
+          'no_tel': '+4176 615 6408'
+          }
+# Entête (ligne 1)
 c = canvas.Canvas("facture_sloffiemusic.pdf", pagesize=A4)
 c.drawImage("logo.jpg", 30, h - 70, width=100, height=45)
-c.setFont("Times-Roman", 48)
-c.drawString(w-180, h-60, "Facture")
-c.line(30, h-100, w-30, h-100)
-c.setFont("Times-Bold", 16)
-c.drawString(30, h-120, "Sloffiemusic-Coaching vocal")
-c.setFont("Times-Roman", 14)
-c.drawString(30, h-136, "My Fun Music School")
-c.drawString(30, h-152, "Route de Chêne 78")
-c.setFont("Times-Roman", 14)
-c.drawString(w-195, h-120, "no facture:")
-c.drawString(w-195, h-136, "date facture:")
-c.drawString(w-195, h-152, "échéance facture:")
-c.drawRightString(w-30, h-120, "FACT_001")
-c.drawRightString(w-30, h-136, "24.04.2023")
-c.drawRightString(w-30, h-152, "24.05.2023")
-c.line(30, h-168, w-30, h-168)
-c.setFont("Times-Bold", 14)
-c.drawString(30, h-190, "Facture à:")
-c.setFont("Times-Roman", 14)
-c.drawString(100, h-190, "Mr. Marc Wanner")
-c.drawString(100, h-206, "141 chemin de l'Eau Belle")
-c.drawString(100, h-222, "74100 Etrembières, France")
-c.drawString(100, h-238, "marcram@proton.me")
-c.line(30, h-250, w-30, h-250)
+c.line(135, h-56, w-25, h-56)
+c.setFont("Times-Roman", 20)
+c.drawString(w-220, h-45,f"Facture: {no_fact}")
+# Entête (ligne 2)
+c.setFont("Times-Bold", 12)
+haut = h-88
+c.drawString(30, haut + 1, "Sloffiemusic-Coaching vocal")
+c.setFont("Times-Roman", 12)
+c.drawString(30, haut - 1 * step, "@My FunMusic School")
+c.drawString(30, haut - 2 * step, "Route de Chêne 78")
+c.drawString(30, haut - 3 * step, "1224 Chêne-Bougerie")
+c.drawString(30, haut - 4 * step, "tel: +4176 313 0193")
+c.setFont("Times-Roman", 12)
+c.drawString(w-167, haut, f"date facture:         {date_fact}")
+c.drawString(w-167, haut - 1 * step, f"échéance facture: {date_fact}")
+c.setStrokeGray(0.8)
+c.line(30, h-160, w-30, h-160)
+# Infos client
+haut = h-190
+c.setFont("Times-Bold", 12)
+c.drawString(30, haut, "Facture à:")
+c.setFont("Times-Roman", 12)
+c.drawString(30, haut - 1 * step, f"{client['prenom']} {client['nom']}")
+c.drawString(30, haut - 2 * step, f"{client['adresse']}")
+c.drawString(30, haut - 3 * step, f"{client['code_postal']} {client['ville']}")
+c.drawString(30, haut - 4 * step, f"{client['mail']}")
+c.drawString(30, haut - 5 * step, f"{client['no_tel']}")
+c.line(30, h-280, w-30, h-280)
+c.setFillColorRGB(0.8, 0.8, 0.8)
+c.rect(30, h-280, w-60, -40, fill=1)
 c.showPage()
 c.save()
